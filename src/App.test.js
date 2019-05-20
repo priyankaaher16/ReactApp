5eab0df175bc44b6717cc4d4fd2f57a0e715385d
Priyanka Aher <priyankaaher162@gmail.com>
2019-05-20 14:57:56 +0530

    Test case demo added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import './setupTest';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+// 	const div = document.createElement('div');
+// 	ReactDOM.render(<App />, div);
+// 	ReactDOM.unmountComponentAtNode(div);
+// });
+
+// describe('Examining the syntax of Jest Test', () => {
+// 	it('sum numbers', () => {
+// 		expect(1 + 2).toEqual(3);
+// 		expect(1 + 2 + 6).toEqual(4);
+// 	});
+// });
+
+describe('First React component test with Enzyme', () => {
+	it('renders without crashing', () => {
+		shallow(<App />);
+	});
 });
